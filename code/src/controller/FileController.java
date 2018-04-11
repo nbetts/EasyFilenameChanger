660fba33dfc26b982590e007c914b7bfb1133710
Nathan Betts <nathanbetts@hotmail.co.uk>
2018-04-11 20:47:37 +0100

    Update generateTableData method

--- code/src/controller/FileController.java
+++ code/src/controller/FileController.java
@@ -37,22 +37,25 @@ public class FileController {
     //
   }
 
-  // public String[][] generateTableData() {
-  //   String[][] tableData = new String[fileList.size()][3];
-  //   boolean isUsingFileExtensions = config.getIsUsingFileExtensions();
+  public String[][] generateTableData() {
+    String[][] tableData = new String[fileList.size()][3];
 
-  //   for (int i = 0; i < tableData.length; i++) {
-  //     SystemFile file = fileList.get(i);
-  //     String subDirectory = file.getSubDirectory();
-  //     subDirectory = subDirectory.substring(0, subDirectory.length() - 1);
+    int currentDirectoryLength = configController.config.getCurrentDirectory().length();
+    boolean isUsingFileExtensions = configController.config.getIsUsingFileExtensions();
 
-  //     tableData[i][0] = subDirectory.substring(subDirectory.lastIndexOf(FILE_SEPARATOR) + 1);
-  //     tableData[i][1] = file.getName(isUsingFileExtensions);
-  //     tableData[i][2] = "";
-  //   }
+    for (int i = 0; i < tableData.length; i++) {
+      File file = fileList.get(i);
+      String parentDirectory = file.getPath().toString();
+      parentDirectory = parentDirectory.substring(currentDirectoryLength,
+                                        parentDirectory.lastIndexOf(FILE_SEPARATOR));
 
-  //   return tableData;
-  // }
+      tableData[i][0] = parentDirectory;
+      tableData[i][1] = file.getName(isUsingFileExtensions);
+      tableData[i][2] = "";
+    }
+
+    return tableData;
+  }
 
   // public void addNewFilenames(String[][] tableData) {
   //   for (int i = 0; i < tableData.length; i++) {
