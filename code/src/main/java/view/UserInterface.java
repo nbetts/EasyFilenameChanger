94e78dc245205953a3a61115d6b228e12b1ab467
Nathan Betts <nathanbetts@hotmail.co.uk>
2018-04-16 21:06:07 +0100

    Add tooltips for table cells to display whole strings

--- code/src/main/java/view/UserInterface.java
+++ code/src/main/java/view/UserInterface.java
@@ -7,12 +7,15 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.Label;
+import javafx.scene.control.TableCell;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.Tooltip;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.DirectoryChooser;
 import javafx.stage.Stage;
+import javafx.util.Callback;
 
 import controller.FileController;
 import model.File;
@@ -61,6 +64,34 @@ public class UserInterface extends Application {
   private void updateTable() {
     fileTable.getItems().setAll(fileController.getFileList());
     fileTable.sort();
+
+    int counter = 0;
+
+    for (TableColumn<File, ?> column : fileTable.getColumns()) {
+      addTooltipToColumnCells(column, counter == 2);
+      counter++;
+    }
+  }
+
+  // Adapted from https://stackoverflow.com/a/40677710.
+  private <T> void addTooltipToColumnCells(TableColumn<File, T> column, boolean isNewNameColumn) {
+    String newNameTooltip = "Double click to edit new name.";
+
+    Callback<TableColumn<File, T>, TableCell<File, T>> existingCellFactory = column.getCellFactory();
+
+    column.setCellFactory(c -> {
+      TableCell<File, T> cell = existingCellFactory.call(c);
+      Tooltip tooltip = new Tooltip();
+
+      if (isNewNameColumn) {
+        tooltip.setText(newNameTooltip);
+      } else {
+        tooltip.textProperty().bind(cell.itemProperty().asString());
+      }
+
+      cell.setTooltip(tooltip);
+      return cell;
+    });
   }
 
   @Override
